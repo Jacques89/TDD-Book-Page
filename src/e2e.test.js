dc81ff88dc06ba77f57e21c0364be083ff65c00a
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2020-04-01 18:40:50 +0200

    Added BookListContainer and BookList.js

--- src/e2e.test.js
+++ src/e2e.test.js
@@ -1,4 +1,5 @@
 import puppeteer from 'puppeteer'
+import axios from 'axios'
 
 const appUrlBase = 'http://localhost:3000'
 
@@ -11,14 +12,40 @@ beforeAll(async () => {
 })
 
 describe('Bookish', () => {
+    afterEach(() => {
+        return axios.delete('http://localhost:8080/books?_cleanup=true').catch(err => err)
+    })
+
+    beforeEach(() => {
+        const books = [
+            {"name": "Refactoring", "id": 1},
+            {"name": "Domain-driven design", "id": 2},
+            {"name": "Building Micro-service", "id": 3}
+        ]
+    
+        return books.map(item => axios.post('http://localhost:8080/books', item, {headers: { 'Content-Type': 'application/json' }}))
+    })
+
     test('Heading', async () => {
         await page.goto(`${appUrlBase}/`)
         await page.waitForSelector('h1')
         const result = await page.evaluate(() => {
-            return document.querySelector('h1').innerText
+          return document.querySelector('h1').innerText
+        })
+        expect(result).toEqual('Bookish')
+    })
+
+    test('Book List', async () => {
+        await page.goto(`${appUrlBase}/`)
+        await page.waitForSelector('.books')
+        const books = await page.evaluate(() => {
+            return [...document.querySelectorAll('.book .title')].map(el => el.innerText)
         })
 
-        expect(result).toEqual('Bookish')
+        expect(books.length).toEqual(3)
+        expect(books[0]).toEqual('Refactoring')
+        expect(books[1]).toEqual('Domain-driven design')
+        expect(books[2]).toEqual('Building Micro-service')
     })
 })
 
